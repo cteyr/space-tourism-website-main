1d848b02b115e88a6bd3f1bb4db98891f82fc9ed
cteyr123 <cteyr123@gmail.com>
2022-08-05 00:40:09 +0200

    navbar style add

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,36 @@
+import { NavLink } from "react-router-dom";
+//@ts-ignore
+import logo from "../assets/images/shared/logo.svg";
 const Navbar = () => {
   return (
     <div className="header">
-      <div className="tittle">
-        <h1>Hola</h1>
+      <img className="logo-image" src={logo} alt="" />
+      <div className="navbar">
+        <nav>
+          <ul>
+            <li>
+              <NavLink className={"nameLink"} to="/">
+                <span> 01 </span> Home
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink className={"nameLink"} to="/destination">
+                <span> 02 </span> Destination
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={"nameLink"} to="/cew">
+                <span> 03 </span> Crew
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={"nameLink"} to="/technology">
+                <span> 04 </span> Technology
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
