c6e0eb949eddceb3138fba0901d7b15c52f225b4
cteyr123 <cteyr123@gmail.com>
2022-08-05 11:10:06 +0200

    text container left add

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -7,6 +7,21 @@ const HomeContainer = () => {
       <div className="background-image">
         <img src={home_background} alt="" />
       </div>
+      <div className="mainContainer">
+        <div className="left-container">
+          <div className="text-container">
+            <h2>SO, YOU WANT TO TRAVEL TO</h2>
+            <h1>SPACE</h1>
+            <p>
+              Let’s face it; if you want to go to space, you might as well
+              genuinely go to outer space and not hover kind of on the edge of
+              it. Well sit back, and relax because we’ll give you a truly out of
+              this world experience!
+            </p>
+          </div>
+        </div>
+        <div className="right-container"></div>
+      </div>
     </div>
   );
 };
