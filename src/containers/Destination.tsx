7d08a4988c8c349fc5a6dbe63f7a05d853e414f7
Carlos Tey <ctey@divisait.com>
2022-08-20 15:32:51 +0200

    imagePlanet add in destination

--- src/containers/Destination.tsx
+++ src/containers/Destination.tsx
@@ -1,8 +1,24 @@
-import exports from "webpack";
+import { useState } from "react";
+import dataJson from "../data.json"
 //@ts-ignore
-//import destination_background from "../assets/images/destination/background-destination-desktop.jpg";
+
 const Destination = () => {
-  return <div className="destinationContainer"></div>;
+  const [destinationData,setdestinationData]= useState([dataJson.destinations])
+  const imageMars=destinationData[0][1];
+  
+  return <div className="destinationContainer">
+    <div className="mainContainer">
+      <div className="left-container">
+        <div className="text-container"><p><span>01</span>PICK YOUR DESTINATION</p>
+        <div className="imageMoon"></div>
+        <div className="imageMars"></div>
+        <div className="imageEuropa"></div>
+        <div className="imageTitan"></div>
+        </div>
+      </div>
+      <div className="right-container"></div>
+    </div>
+  </div>;
 };
 
 export { Destination };
