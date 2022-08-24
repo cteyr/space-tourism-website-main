91bce6eb1a77cfd118c372db28c179ed49f0b609
Carlos Tey <ctey@divisait.com>
2022-08-24 18:35:00 +0200

    Add image to right container

--- src/containers/Crew.tsx
+++ src/containers/Crew.tsx
@@ -2,7 +2,16 @@
 //import crew_background from "../assets/images/crew/background-crew-desktop.jpg";
 
 const Crew = () => {
-  return <div className="crewContainer"></div>;
+  return(
+   <div className="crewContainer">
+      <div className="mainContainer">
+        <div className="left-container"></div>
+        <div className="right-container">
+          <div className="imagePesonage"></div>
+        </div>
+      </div>
+  </div>
+  )
 };
 
 export { Crew };
