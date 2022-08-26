0fea8efeea12008016986352fd5cb19e844bcdaf
Carlos Tey <ctey@divisait.com>
2022-08-26 19:37:27 +0200

    left-container and tittle add

--- src/containers/Crew.tsx
+++ src/containers/Crew.tsx
@@ -5,7 +5,12 @@ const Crew = () => {
   return(
    <div className="crewContainer">
       <div className="mainContainer">
-        <div className="left-container"></div>
+        <div className="left-container">
+          <div className="text-container">
+            <div className="tittle-container"><p><span>01</span>PICK YOUR DESTINATION</p>
+            </div>
+          </div>
+        </div>
         <div className="right-container">
           <div className="imagePesonage"></div>
         </div>
